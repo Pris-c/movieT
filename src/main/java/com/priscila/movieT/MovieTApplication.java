fba36dbef28ddfbdda5fa567f1afbced25ef4126
Pris-c <priscila.campoos@yahoo.com.br>
2023-07-25 19:24:56 +0100

    Estrutura do Projeto

---
+++ src/main/java/com/priscila/movieT/MovieTApplication.java
@@ -0,0 +1,13 @@
+package com.priscila.movieT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTApplication.class, args);
+	}
+
+}
